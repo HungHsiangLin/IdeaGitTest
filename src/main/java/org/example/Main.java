8906ff6e2db1d96a96fa996aa41771a51bb4f719
Hsiang <d9971322@gmail.com>
2023-01-03 16:10:03 +0800

    第一次修改

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        //字串
+        String[] messages = new String[]{"Sam", "Tom", "You"};
+
+        for (String message : messages) {
+        }
+
     }
 }
